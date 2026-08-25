89edd5f58a17ed439e89af8df4cdd04eb61455dd
Pablo Gutierrez Perez <PAblo-GP@live.com>
2026-08-25 16:58:10 +0100

    Add pandas import to streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,9 @@
 # Import python packages
 import streamlit as st
+import pandas as pd
 from snowflake.snowpark.functions import col
 import requests
+
 
 # Write directly to the app
 st.title(f":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
